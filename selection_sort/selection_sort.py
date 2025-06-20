212cb5fc0b42d156e4aa588188a6d8e40ed0ba6e
David Njagi <njaginmungai@gmail.com>
2025-06-20 13:00:03 +0300

    first commit

--- selection_sort/selection_sort.py
+++ selection_sort/selection_sort.py
@@ -1,3 +1,18 @@
 def selection_sort(arr):
     # TODO: Implement selection sort
-    pass
+    n = len(arr)
+    
+    for i in range(n):
+        min_idx = i
+        for j in range(i+1, n):
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+    return arr
+
+
+data = [14, 12, 11, 67, 98]
+sorted_data = selection_sort(data)
+print(sorted_data)
+
+
